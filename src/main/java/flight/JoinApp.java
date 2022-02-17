382d494c1b53b22e6206c7a36ea0d159592ddb00
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-17 23:44:22 +0300

    Scripted auto-commit on change (2022-02-17 23:44:22) by gitwatch.sh

--- src/main/java/flight/JoinApp.java
+++ src/main/java/flight/JoinApp.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
+import
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class JoinApp {
@@ -20,7 +21,7 @@ public class JoinApp {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-
+        job.setMapOutputKeyClass(TextPair.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
