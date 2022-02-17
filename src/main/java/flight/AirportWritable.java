a03c33ccb641417f8391f3464671c74362b0e9d2
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-17 23:38:31 +0300

    Scripted auto-commit on change (2022-02-17 23:38:31) by gitwatch.sh

--- src/main/java/flight/AirportWritable.java
+++ src/main/java/flight/AirportWritable.java
@@ -1,4 +1,18 @@
 package flight;
 
-public class AirportWritable {
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirportWritable implements WritableComparable {
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    public void write(DataOutput data) throws IOException {
+
+    }
+
+    public
 }
