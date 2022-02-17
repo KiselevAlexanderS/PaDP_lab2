45a1a69e0bfba40e6cd490b74c8e93fa54ae090e
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-18 00:18:11 +0300

    Scripted auto-commit on change (2022-02-18 00:18:11) by gitwatch.sh

--- src/main/java/flight/AirportComparator.java
+++ src/main/java/flight/AirportComparator.java
@@ -1,9 +1,15 @@
 package flight;
 
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportComparator extends WritableComparator {
     protected AirportComparator() {
 
+    }
+
+    @Override
+    public int compare(WritableComparable x, WritableComparable y) {
+        
     }
 }
