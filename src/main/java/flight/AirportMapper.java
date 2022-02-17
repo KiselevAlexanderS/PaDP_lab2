565e546f7de9b6503576a821dc556448790641d8
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-18 00:08:53 +0300

    Scripted auto-commit on change (2022-02-18 00:08:53) by gitwatch.sh

--- src/main/java/flight/AirportMapper.java
+++ src/main/java/flight/AirportMapper.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
     @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException {
+    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException {
         String line = value.toString();
         String[] words = line.split(",[\"]");
         if (key.get() > 0){
