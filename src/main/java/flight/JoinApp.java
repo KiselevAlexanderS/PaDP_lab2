01767fb95977d249646c6bea4440aee0c2ba3178
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-17 23:43:10 +0300

    Scripted auto-commit on change (2022-02-17 23:43:10) by gitwatch.sh

--- src/main/java/flight/JoinApp.java
+++ src/main/java/flight/JoinApp.java
@@ -23,6 +23,7 @@ public class JoinApp {
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
