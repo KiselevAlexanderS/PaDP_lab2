83d1961b65ad4a4cf956e1b61637dd3674958eb1
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-18 00:29:11 +0300

    Scripted auto-commit on change (2022-02-18 00:29:11) by gitwatch.sh

--- src/main/java/flight/AirportReducer.java
+++ src/main/java/flight/AirportReducer.java
@@ -3,6 +3,24 @@ package flight;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+import java.util.Iterator;
+
+
 public class AirportReducer extends Reducer<AirportWritable, Text, Text, Text> {
-    protected void reduce()
+    protected void reduce(AirportWritable key, Iterable<Text> val, Context context) throws IOException {
+        long count = 0;
+        float sum = 0;
+        float min = 0;
+        float max = Float.MAX_VALUE;
+        Iterator iterator = values.iterator();
+        Text name = new Text(iterator.next().toString());
+        while (iterator.hasNext()){
+            float delay = Float.parseFloat(iterator.next().toString());
+            sum += delay;
+            count++;
+            max = Math.max(delay,max);
+            min = Math.min(delay,min);
+        }
+    }
 }
