30abae01eeb8c4eb11c0aec78cae105c17b3aaec
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-17 03:40:34 +0300

    Scripted auto-commit on change (2022-02-17 03:40:34) by gitwatch.sh

--- src/main/java/flight/JoinApp.java
+++ src/main/java/flight/JoinApp.java
@@ -1,10 +1,13 @@
 package flight;
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class JoinApp {
+    public static void main(String[] args) throws Exception {
+        
+    }
 }
