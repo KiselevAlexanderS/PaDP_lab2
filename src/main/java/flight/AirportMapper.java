1c7e1b575db31d6c54671b90b35136133307fa15
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-18 00:00:05 +0300

    Scripted auto-commit on change (2022-02-18 00:00:05) by gitwatch.sh

--- src/main/java/flight/AirportMapper.java
+++ src/main/java/flight/AirportMapper.java
@@ -11,6 +11,6 @@ public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable>
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException {
         String line = value.toString();
-        
+        String[] words = line.split(",[\"]");
     }
 }
