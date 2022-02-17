195b777227e93ac7950f54c8d5f63fcf27ad26a8
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-17 23:56:57 +0300

    Scripted auto-commit on change (2022-02-17 23:56:57) by gitwatch.sh

--- src/main/java/flight/FlightMapper.java
+++ src/main/java/flight/FlightMapper.java
@@ -3,6 +3,7 @@ package flight;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 
 public class FlightMapper extends Mapper<LongWritable, Text> {
 
