9bdb85193727923c02528bfe26d934624b62dc02
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-18 00:11:12 +0300

    Scripted auto-commit on change (2022-02-18 00:11:12) by gitwatch.sh

--- src/main/java/flight/FlightMapper.java
+++ src/main/java/flight/FlightMapper.java
@@ -1,10 +1,17 @@
 package flight;
 
 
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class FlightMapper extends Mapper<LongWritable, Text> {
+import java.io.IOException;
 
+public class FlightMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+        int air_id_col = 14;
+        int air_delay_col = 18;
+    }
 }
